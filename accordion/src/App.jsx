0e6c65b4b48afa11393405bf5d1b5916f12bdb2b
Ron Matthew Alunan <ron.alunan@usjr.edu.ph>
2022-10-24 01:38:18 +0800

    accordion exercise

---
+++ accordion/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import data from './data';
+import Question from './Question';
+
+const App = () => {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (<Question key={question.id} {...question} />);
+          })}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App;
