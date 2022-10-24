7eed11ef6986d9980a083ab0fbc3abf0ca1b4f83
Ron Matthew Alunan <ron.alunan@usjr.edu.ph>
2022-10-24 20:41:43 +0800

    implemented experience exercise

---
+++ experience/src/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <section className="section loading">
+      <h1>loading...</h1>
+    </section>
+  );
+};
+
+export default Loading;
